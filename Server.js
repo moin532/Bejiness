541345f62a9d95d660aa7415b0a993cfcd71d046
SumithRaj05 <sumithrajpurohit03+1@gmail.com>
2024-06-09 19:18:35 +0530

    fixed bugs

--- Server.js
+++ Server.js
@@ -19,18 +19,23 @@ app.use(express.static(buildPath));
 
 // Middlewares
 
+// Define allowed origins
+const allowedOrigins = [
+    'https://bejiness.in',
+    'http://bejiness.in'
+];
+
+// CORS configuration
 app.use(cors({
-    "origin": "*",
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
 }));
-app.use((req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://bejiness.in/');
-    res.setHeader('Access-Control-Allow-Origin', 'http://bejiness.in/');
-
-    res.setHeader('Access-Control-Allow-Origin', 'https://bejiness.in/*');
-
-    res.setHeader('Access-Control-Allow-Origin', 'http://bejiness.in/*');
-    next();
-})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
